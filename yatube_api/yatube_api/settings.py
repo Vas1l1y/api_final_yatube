590493a6b6984a37d6be7ad1c765832fcc4ed8e4
Василий Ларькин <larkin-vk@mail.ru>
2022-09-18 19:12:44 +0300

    Project 8. V.1.0

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -99,5 +99,5 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
-   'AUTH_HEADER_TYPES': ('Bearer',), }
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
+    'AUTH_HEADER_TYPES': ('Bearer',), }
